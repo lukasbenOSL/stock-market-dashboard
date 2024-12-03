00790d8696f35bc46f91829f191189fb1f47e64e
lukasbenOSL <benner.lukas@gmail.com>
2024-12-03 18:41:17 +0100

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StockDashboard from './components/Dashboard/StockDashboard';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <StockDashboard />
+    </div>
+  );
+}
